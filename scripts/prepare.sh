031d8aca0d753b63288bc50ee641b3b3e8477846
ZixunH <zixun@berkeley.edu>
2025-04-15 23:49:17 -0700

    refine eq preparation, fov comparison

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,10 +1,12 @@
+SCENE_ID=1f7cbbdde1
+DATASET_DIR=/media/scannetpp/demo/$SCENE_ID/dslr
+
 python prepare_scannetpp.py \
-    --path /media/scannetpp/demo/0a5c013435/dslr/ \
+    --path $DATASET_DIR \
     --src resized_images \
     --dst image_undistorted_fisheye 
 
-
 python prepare_scannetpp_fish2equi.py \
-    --path /media/scannetpp/demo/0a5c013435/dslr/ \
+    --path $DATASET_DIR \
     --src resized_images \
     --dst images_equidist
