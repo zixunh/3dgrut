a058c37d93d28637d440955315503b1915f1519e
ZixunH <zixun@berkeley.edu>
2025-04-15 23:47:44 -0700

    docker shortcut

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,13 +1,13 @@
-SCENE_ID=0a5c013435
-OUTPUT_DIR=/workspace/runs/"$SCENE_ID"_3dgut/dslr-1604_045030
+SCENE_ID=1f7cbbdde1
+OUTPUT_DIR=/workspace/runs/"$SCENE_ID"_3dgut/dslr-1604_063449
 DATASET_DIR=/media/scannetpp/demo/$SCENE_ID/dslr
 ITERS_NUM=30000
 
-# # wrap back to origianal space
-# python prepare_scannetpp_equi2fish.py \
-#     --camera-path $DATASET_DIR/colmap/cameras_equidist.txt \
-#     --src $OUTPUT_DIR/ours_30000/renders \
-#     --dst $OUTPUT_DIR/ours_30000/renders_remap \
+# wrap back to origianal space
+python prepare_scannetpp_equi2fish.py \
+    --camera-path $DATASET_DIR/colmap/cameras_equidist.txt \
+    --src $OUTPUT_DIR/ours_30000/renders \
+    --dst $OUTPUT_DIR/ours_30000/renders_remap \
 
 # evaluation
 python metrics.py \
--- scripts/run_scannetpp.sh
+++ scripts/run_scannetpp.sh
@@ -1,9 +1,9 @@
-SCENE_ID=2a1a3afad9
+SCENE_ID=1f7cbbdde1
 DATASET_DIR=/media/scannetpp/demo/$SCENE_ID/dslr
 
-# python prepare_scannetpp_fish2equi.py \
-#     --path $DATASET_DIR \
-#     --src resized_images \
-#     --dst images_equidist
+python prepare_scannetpp_fish2equi.py \
+    --path $DATASET_DIR \
+    --src resized_images \
+    --dst images_equidist
 
 python train.py --config-name apps/scannetpp_3dgut.yaml path=$DATASET_DIR out_dir=runs experiment_name="$SCENE_ID"_3dgut
